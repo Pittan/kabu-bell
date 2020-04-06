2a4fa5810dc9fe961c9d5be5b614034f09e7d9fe
Pittan <pittan@pittankopta.net>
2020-04-06 23:19:14 +0900

    Fix building issue

--- src/shared/check-for-update.service.ts
+++ src/shared/check-for-update.service.ts
@@ -2,6 +2,7 @@ import { ApplicationRef, Injectable } from '@angular/core'
 import { SwUpdate } from '@angular/service-worker'
 import { concat, interval } from 'rxjs'
 import { first } from 'rxjs/operators'
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class CheckForUpdateService {
     public appRef: ApplicationRef,
     public updates: SwUpdate
   ) {
-    if (process.env.NODE_ENV === 'development') return
+    if (!environment.production) return
     // Allow the app to stabilize first, before starting polling for updates with `interval()`.
     const appIsStable$ = appRef.isStable.pipe(first(isStable => isStable === true))
     const everySixHours$ = interval(6 * 60 * 60 * 1000)
